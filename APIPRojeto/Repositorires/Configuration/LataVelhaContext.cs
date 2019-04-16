48a9a3eedb60669207e403fdbe4e51c8277e0000
ldbs <leonardo.barros@gft.com>
2019-04-16 10:14:26 -0300

    Configurando o contexto

--- APIPRojeto/Repositorires/Configuration/LataVelhaContext.cs
+++ APIPRojeto/Repositorires/Configuration/LataVelhaContext.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APIPRojeto.Repositorires.Configuration
 {
-    public class LataVelhaContext
+    public class LataVelhaContext : DbContext 
     {
+        public LataVelhaContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = @"localhost\\SQLEXPRESS;Database=LataVelha;Trusted_Connection=true";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
     }
 }
