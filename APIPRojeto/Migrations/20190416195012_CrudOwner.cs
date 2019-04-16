51358ac355e32d28f311cf348e68851336062aac
aera <aera@BRPC003891>
2019-04-16 16:50:53 -0300

    Crud Owner

---
+++ APIPRojeto/Migrations/20190416195012_CrudOwner.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APIPRojeto.Migrations
+{
+    public partial class CrudOwner : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
