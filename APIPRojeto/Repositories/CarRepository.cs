602c686b9894f1b425b1e848598000ab36dcd6c5
ViniciusLeme <Vinicius.Pierina@gft.com>
2019-04-16 10:35:16 -0300

    Criando Repositório (Car)

---
+++ APIPRojeto/Repositories/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using APIPRojeto.Models;
+using APIPRojeto.Repositories.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPRojeto.Repositories
+{
+
+    public class CarRepository
+    {
+        private readonly LataVelhaContext _db;
+
+        public CarRepository()
+        {
+            _db = new LataVelhaContext();
+        }
+
+        public IEnumerable<Car> Get() => _db.Cars.ToList();
+    }
+}
