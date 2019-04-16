91f9c62486f52928a9c82402a28e704e810c9867
Paulo leme <pojn@BRPC003897>
2019-04-16 14:41:53 -0300

    Adicionando Repositório (Car)

---
+++ APIPRojeto/Repositorio/CarRepository.cs
@@ -0,0 +1,28 @@
+﻿using APIPRojeto.Models;
+using APIPRojeto.Repositorio.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APIPRojeto.Repositorio
+{
+
+
+    public class CarRepository
+        
+    {
+        private readonly LataVelhaContext _db;
+
+        public CarRepository()
+        {
+            _db = new LataVelhaContext();
+        }
+
+        public IEnumerable<Car> Get() => _db.Cars.ToList();
+
+
+
+
+
+
+    }
+}
--- APIPRojeto/Repositorio/Configuration/LataVelhacontext.cs
+++ APIPRojeto/Repositorio/Configuration/LataVelhacontext.cs
@@ -3,11 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using APIPRojeto.Models;
 
 namespace APIPRojeto.Repositorio.Configuration
 {
     public class LataVelhaContext : DbContext
     {
+        public DbSet <Car> Cars { get; set; }
+
         public LataVelhaContext()
         {
 
@@ -18,7 +22,7 @@ namespace APIPRojeto.Repositorio.Configuration
             if (!optionsBuilder.IsConfigured)
             {
                 var connectionString = @"Server=BRPC003897; Database=LataVelha; Trusted_Connection=True";
-        optionsBuilder.UseSqlServer(connectionString);
+                optionsBuilder.UseSqlServer(connectionString);
 
             }
            }
