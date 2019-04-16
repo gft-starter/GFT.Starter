35a02dc19453b435911751bdfd365633392ea14a
aiml150 <ariel.molina@gft.com>
2019-04-16 10:16:58 -0300

    Configuração inicial do contexto.

---
+++ APIPRojeto/Repositories/Configuration/LataNovaContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPRojeto.Repositories.Configuration
+{
+    public class LataNovaContext : DbContext
+    {
+        protected LataNovaContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = @"Server=BRPC003931\SQLEXPRESS150;Database=LataNova;Trusted_Connection=True";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
