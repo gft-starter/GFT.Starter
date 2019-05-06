8d9e6315814fbee9de864420514870ca67393718
ViniciusLeme <Vinicius.Pierina@gft.com>
2019-05-06 13:13:07 -0300

    Dividindo as APIs em micro-services

---
+++ API_Services/Controllers/ServiceController.cs
@@ -0,0 +1,70 @@
+﻿using GFT.Starter.Core.Models;
+using GFT.Starter.Infrastructure.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace API_Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IReadOnlyRepository<Service> _serviceReadOnlyRepository;
+        private readonly IWriteRepository<Service> _serviceWriteRepository;
+
+        public ServiceController(IReadOnlyRepository<Service> serviceReadOnlyRepository, IWriteRepository<Service> serviceWriteRepository)
+        {
+            _serviceReadOnlyRepository = serviceReadOnlyRepository;
+            _serviceWriteRepository = serviceWriteRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Services()
+        {
+            return Ok(_serviceReadOnlyRepository.Get());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult Service(Guid id)
+        {
+            var obj = FindService(id);
+            return Ok(obj);
+        }
+
+        [HttpPost]
+        public IActionResult PostService([FromBody] Service service)
+        {
+            _serviceWriteRepository.Add(service);
+
+            return Ok(service);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateService(Guid id, [FromBody] Service service)
+        {
+            var obj = FindService(id);
+
+            obj.Name = service.Name;
+
+            return Ok(obj);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteService(Guid id)
+        {
+            var obj = FindService(id);
+
+            if (obj != null)
+                return Ok(_serviceWriteRepository.Remove(obj));
+
+            return NotFound(obj);
+        }
+
+        private Service FindService(Guid id)
+        {
+            return _serviceReadOnlyRepository.Find(id);
+        }
+    }
+}
---
+++ API_Services/Controllers/ServiceOrderController.cs
@@ -0,0 +1,69 @@
+﻿using GFT.Starter.Core.Models;
+using GFT.Starter.Infrastructure.Repositories.Contracts;
+using GFT.Starter.Infrastructure.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace API_Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceOrderController : ControllerBase
+    {
+        private readonly IReadOnlyRepository<ServiceOrder> _serviceOrdeReadOnlyRepository;
+        private readonly IWriteRepository<ServiceOrder> _serviceOrderWriteRepository;
+        private readonly IServiceOrderCalculator _serviceOrderCalculator;
+
+        public ServiceOrderController(IReadOnlyRepository<ServiceOrder> serviceOrdeReadOnlyRepository, IWriteRepository<ServiceOrder> serviceOrderWriteRepository, IServiceOrderCalculator serviceOrderCalculator)
+        {
+            _serviceOrdeReadOnlyRepository = serviceOrdeReadOnlyRepository;
+            _serviceOrderWriteRepository = serviceOrderWriteRepository;
+            _serviceOrderCalculator = serviceOrderCalculator;
+        }
+
+        [HttpGet]
+        public IActionResult ServiceOrders()
+        {
+            return Ok(_serviceOrdeReadOnlyRepository.Get());
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult ServiceOrder(Guid id)
+        {
+            var obj = FindServiceOrder(id);
+            return Ok(obj);
+        }
+
+        [HttpPost]
+        public IActionResult PostServiceOrder([FromBody] ServiceOrder serviceOrder)
+        {
+            _serviceOrderWriteRepository.Add(serviceOrder);
+
+            return Ok(serviceOrder);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateServiceOrder(Guid id, [FromBody] ServiceOrder serviceOrder)
+        {
+            var obj = FindServiceOrder(id);
+
+            obj.Quantity = serviceOrder.Quantity;
+            _serviceOrderWriteRepository.Update(obj);
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Calculate")]
+        public IActionResult CalculatePrice(Guid id)
+        {
+            var obj = FindServiceOrder(id);
+            return Ok(_serviceOrderCalculator.CalculateTotalPrice(obj));
+        }
+
+        private ServiceOrder FindServiceOrder(Guid id)
+        {
+            return _serviceOrdeReadOnlyRepository.Find(id);
+        }
+    }
+}
