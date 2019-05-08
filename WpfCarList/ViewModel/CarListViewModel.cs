f0eba6d7e4886d7f99ac2e2093b3d67b7412c2a1
ldbs <ldbs@gft.com>
2019-05-08 15:04:37 -0300

    WPFCarList

--- WpfCarList/ViewModel/CarListViewModel.cs
+++ WpfCarList/ViewModel/CarListViewModel.cs
@@ -11,9 +11,47 @@ namespace WpfCarList.ViewModel
             ListarCarros();
         }
 
-        public void ListarCarros()
+        private void ListarCarros()
         {
-           
+            Add(new Car
+            {
+                Id = Guid.NewGuid(),
+                Brand = "Chevrolet",
+                Color = "Black",
+                Model = "Celta",
+                Plate = "HDJ-2407",
+                Year = 2009
+            });
+
+            Add(new Car
+            {
+                Id = Guid.NewGuid(),
+                Brand = "Ford",
+                Color = "Blue",
+                Model = "Focus",
+                Plate = "DTY-5567",
+                Year = 2013
+            });
+
+            Add(new Car
+            {
+                Id = Guid.NewGuid(),
+                Brand = "Fiat",
+                Color = "Green",
+                Model = "Uno",
+                Plate = "FKY-7489",
+                Year = 1998
+            });
+
+            Add(new Car
+            {
+                Id = Guid.NewGuid(),
+                Brand = "Volkswagen",
+                Color = "Blue",
+                Model = "Fusca",
+                Plate = "FMI-1458",
+                Year = 1969
+            });
         }
     }
 }
