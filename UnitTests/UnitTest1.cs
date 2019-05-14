50ffbdb64354db0b9a33713eccf8233af7742519
Luis Trochmann <lztn@gft.com>
2019-05-14 19:57:01 -0300

    wip

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,278 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod001()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod002()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod003()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod004()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod005()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod006()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod007()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod008()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod009()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod010()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod011()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod012()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod013()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod014()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod015()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod016()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod017()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod018()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod019()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod020()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod021()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod022()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod023()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod024()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod025()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod026()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod027()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod028()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod029()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod030()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod031()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod032()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod033()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod034()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod035()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod036()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod037()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod038()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod039()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod040()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod041()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod042()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod043()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod044()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod045()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
