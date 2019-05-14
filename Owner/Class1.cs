23910d591b2c37b332a594fdeb4cc707d6f32031
pauloleme <paulolemejr1@gmail.com>
2019-05-14 11:53:12 -0300

    Teste

--- Owner/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿    using System;
-
-namespace Owner
-{
-    public class Class1
-    {
-    }
-}
--- OwnerProject/3 - Infrastructure/Repositories/Contracts/IWriteRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GFT.Starter.Infrastructure.Repositories.Contracts
-{
-    public interface IWriteRepository<T>
-    {
-        void Add(T car);
-        T Remove(T car);
-        T Update(T car);
-    }
-}
